b18a82d221cffa7cd5e760f5a99718cf90df06a2
Diego <diego.vid.eco@gmail.com>
2021-12-18 23:22:44 -0600

    Add functions for writing tuning files in afable-diablo

--- src/tieminos/afable_diablo/harmonic_form.clj
+++ src/tieminos/afable_diablo/harmonic_form.clj
@@ -38,6 +38,18 @@
     :hexs [[1 3 9 19] [1 3 7 9]],
     :name "3)5 of 4)7 15-1.3.7.9.19"}])
 
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Write tunings files
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
 (def tunings-dir
   (let [dir-path (-> (str (ns-name *ns*)) (str/split #"\.")
                      (->> (drop-last 1) (str/join "/"))
@@ -49,55 +61,56 @@
 (defn +sc-tun-note [scale]
   (map #(assoc % :sc/tun-note (/ (:cents %) 100))
        scale))
-(do
-  (defn get-scale [moment]
-    (let [name* (moment :name)]
-      [moment
-       (if (moment :scale)
-         (+sc-tun-note (moment :scale))
-         (-> polydori :subcps (get name*) :scale +cents
-             (->> +sc-tun-note)))]))
 
-  (defn make-sc-tuning [tuning-name-fn [moment scale]]
-    (let [fmt #(format "Tuning.new(#[%s], \"%s\");"
-                       % (tuning-name-fn moment))]
-      (->> (mapv :sc/tun-note scale)
-           (str/join ", " )
-           fmt)))
+(defn get-scale [moment]
+  (let [name* (moment :name)]
+    [moment
+     (if (moment :scale)
+       (+sc-tun-note (moment :scale))
+       (-> polydori :subcps (get name*) :scale +cents
+           (->> +sc-tun-note)))]))
 
-  (defn make-file [file-name sc-tunings]
-    (let [data (format "(\n%s\n);" (str/join "\n" sc-tunings))]
-      (spit (str tunings-dir "/" file-name) data)))
+(defn make-sc-tuning [tuning-name-fn [moment scale]]
+  (let [fmt #(format "Tuning.new(#[%s], \"%s\");"
+                     % (tuning-name-fn moment))]
+    (->> (mapv :sc/tun-note scale)
+         (str/join ", " )
+         fmt)))
 
-  (defn get-hexanies [moment]
-    (let [subcps-names (->> polydori :subcps keys)]
+(defn make-file [file-name sc-tunings]
+  (let [data (format "(\n%s\n);" (str/join "\n" sc-tunings))]
+    (spit (str tunings-dir "/" file-name) data)))
 
-      (->> (moment :hexs)
-           (mapcat (fn [h]
-                     (filter #(and (str/includes? % (str/join "." h))
-                                   (str/includes? % "2)4"))
-                             subcps-names)))
-           set
-           (map #(assoc moment
-                        :scale (-> polydori :subcps (get %) :scale +cents)
-                        :hex/name (->  % (str/split #" ") last))))))
+(defn get-hexanies [moment]
+  (let [subcps-names (->> polydori :subcps keys)]
 
-  (defn dek-tun-name [moment]
-    (str "DekMoment " (moment :momento)))
-  (defn hex-tun-name [moment]
-    (format "HexMoment %s (%s)" (moment :momento) (moment :hex/name)))
-  (def dekany-tunings
-    (->> harmonic-form
-         #_  (drop 1)
-         (map get-scale)
-         (map (partial make-sc-tuning dek-tun-name))
-         (make-file "dekany-moments-tunings.scd")))
-  (def hexany-tunings
-    (->> harmonic-form
-         (drop 1)
-         (mapcat get-hexanies)
-         (map get-scale)
-         (map (partial make-sc-tuning hex-tun-name))
-         (make-file "hexany-moments-tunings.scd")))
-  #_dekany-tunings
-  hexany-tunings)
+    (->> (moment :hexs)
+         (mapcat (fn [h]
+                   (filter #(and (str/includes? % (str/join "." h))
+                                 (str/includes? % "2)4"))
+                           subcps-names)))
+         set
+         (map #(assoc moment
+                      :scale (-> polydori :subcps (get %) :scale +cents)
+                      :hex/name (->  % (str/split #" ") last))))))
+
+(defn dek-tun-name [moment]
+  (str "DekMoment " (moment :momento)))
+(defn hex-tun-name [moment]
+  (format "HexMoment %s (%s)" (moment :momento) (moment :hex/name)))
+
+(comment
+  ;; make-dekany-tunings
+  (->> harmonic-form
+       #_  (drop 1)
+       (map get-scale)
+       (map (partial make-sc-tuning dek-tun-name))
+       (make-file "dekany-moments-tunings.scd"))
+
+  ;; make-hexany-tunings
+  (->> harmonic-form
+       (drop 1)
+       (mapcat get-hexanies)
+       (map get-scale)
+       (map (partial make-sc-tuning hex-tun-name))
+       (make-file "hexany-moments-tunings.scd")))
