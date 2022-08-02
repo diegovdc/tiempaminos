00c35edb43bf8f069bd6bdbcaf53ff080c0a12e8
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-08-02 07:52:45 -0500

    Update clojure version

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.3.618"]
+                 [org.clojure/tools.namespace "1.2.0"]
                  [potemkin "0.4.5"]
                  [org.clojure/data.generators "1.0.0"]
                  [overtone "0.10.6"]
