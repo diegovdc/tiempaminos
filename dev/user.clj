fb8d8c5d48ccc6d22b80032239c8ec2203cefc12
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-11-10 13:40:20 -0600

    Add refresh dirs

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,9 @@
 (ns user
   (:require [taoensso.timbre :as timbre]
-            [clojure.tools.namespace.repl :as repl :refer [refresh]]))
+            [clojure.tools.namespace.repl :as repl :refer [refresh set-refresh-dirs]]))
 
 (timbre/set-level! :info)
+(set-refresh-dirs "src")
 (comment
   (repl/clear)
   ;; FIXME Ya casi funciona solo hay que arreglar el require en tieminos.compositions.garden-earth.synths.granular
