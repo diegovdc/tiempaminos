a933538cb74330d14795e5e9cd1fac9126a20351
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-12-10 13:36:56 -0600

    WIP scale printing

--- src/tieminos/afable_diablo/dorian_scales.clj
+++ src/tieminos/afable_diablo/dorian_scales.clj
@@ -19,192 +19,192 @@
 (def dorico-1v2
   "NOTE this is the best version
   dorico 1 \"2)4 of 4)7 7.15-1.3.9.19\" with added 1st"
-  [{:set #{15 21 19 9},
-    :ratio 1,
-    :bounded-ratio 1,
-    :bounding-period 2,
-    :degree 0,
+  [{:set #{15 21 19 9}
+    :ratio 1
+    :bounded-ratio 1
+    :bounding-period 2
+    :degree 0
     :cents 0.0}
-   {:set #{7 1 15 9},
-    :ratio 1/57,
-    :bounded-ratio 64/57,
-    :bounding-period 2,
+   {:set #{7 1 15 9}
+    :ratio 1/57
+    :bounded-ratio 64/57
+    :bounding-period 2
     :cents 200.5319830023106}
-   {:set #{7 1 15 19},
-    :ratio 1/27,
-    :bounded-ratio 32/27,
-    :bounding-period 2,
+   {:set #{7 1 15 19}
+    :ratio 1/27
+    :bounded-ratio 32/27
+    :bounding-period 2
     :cents 294.1349974038373}
-   {:set #{7 15 19 9},
-    :ratio 1/3,
-    :bounded-ratio 4/3,
-    :bounding-period 2,
+   {:set #{7 15 19 9}
+    :ratio 1/3
+    :bounded-ratio 4/3
+    :bounding-period 2
     :cents 498.04499913461217}
-   {:set #{7 1 15 3},
-    :ratio 1/171,
-    :bounded-ratio 256/171,
-    :bounding-period 2,
+   {:set #{7 1 15 3}
+    :ratio 1/171
+    :bounded-ratio 256/171
+    :bounding-period 2
     :cents 698.5769821369228}
-   {:set #{7 15 3 9},
-    :ratio 1/19,
-    :bounded-ratio 32/19,
-    :bounding-period 2,
+   {:set #{7 15 3 9}
+    :ratio 1/19
+    :bounded-ratio 32/19
+    :bounding-period 2
     :cents 902.4869838676968}
-   {:set #{7 15 3 19},
-    :ratio 1/9,
-    :bounded-ratio 16/9,
-    :bounding-period 2,
+   {:set #{7 15 3 19}
+    :ratio 1/9
+    :bounded-ratio 16/9
+    :bounding-period 2
     :cents 996.0899982692252}])
 
 (def dorico-1v3
   "dorico 1 \"2)4 of 4)7 7.15-1.3.9.19\" with added sharp 1st degree"
-  [{:set #{1 15 3 19},
-    :ratio 1/63,
-    :bounded-ratio 64/63,
-    :bounding-period 2,
-    :degree 1,
+  [{:set #{1 15 3 19}
+    :ratio 1/63
+    :bounded-ratio 64/63
+    :bounding-period 2
+    :degree 1
     :cents 27.264091800100516}
-   {:set #{7 1 15 9},
-    :ratio 1/57,
-    :bounded-ratio 64/57,
-    :bounding-period 2,
+   {:set #{7 1 15 9}
+    :ratio 1/57
+    :bounded-ratio 64/57
+    :bounding-period 2
     :cents 200.5319830023106}
-   {:set #{7 1 15 19},
-    :ratio 1/27,
-    :bounded-ratio 32/27,
-    :bounding-period 2,
+   {:set #{7 1 15 19}
+    :ratio 1/27
+    :bounded-ratio 32/27
+    :bounding-period 2
     :cents 294.1349974038373}
-   {:set #{7 15 19 9},
-    :ratio 1/3,
-    :bounded-ratio 4/3,
-    :bounding-period 2,
+   {:set #{7 15 19 9}
+    :ratio 1/3
+    :bounded-ratio 4/3
+    :bounding-period 2
     :cents 498.04499913461217}
-   {:set #{7 1 15 3},
-    :ratio 1/171,
-    :bounded-ratio 256/171,
-    :bounding-period 2,
+   {:set #{7 1 15 3}
+    :ratio 1/171
+    :bounded-ratio 256/171
+    :bounding-period 2
     :cents 698.5769821369228}
-   {:set #{7 15 3 9},
-    :ratio 1/19,
-    :bounded-ratio 32/19,
-    :bounding-period 2,
+   {:set #{7 15 3 9}
+    :ratio 1/19
+    :bounded-ratio 32/19
+    :bounding-period 2
     :cents 902.4869838676968}
-   {:set [7 15 3 19],
-    :ratio 1/9,
-    :bounded-ratio 16/9,
-    :bounding-period 2,
+   {:set [7 15 3 19]
+    :ratio 1/9
+    :bounded-ratio 16/9
+    :bounding-period 2
     :cents 996.0899982692252}])
 
 (def dorico-1v3
   "dorico 1 \"2)4 of 4)7 7.15-1.3.9.19\" with added flat 1st degree"
-  [{:set #{7 15 21 3},
-    :ratio 7/57,
-    :bounded-ratio 112/57,
-    :bounding-period 2,
-    :degree 34,
+  [{:set #{7 15 21 3}
+    :ratio 7/57
+    :bounded-ratio 112/57
+    :bounding-period 2
+    :degree 34
     :cents 1169.3578894714346}
-   {:set #{7 1 15 9},
-    :ratio 1/57,
-    :bounded-ratio 64/57,
-    :bounding-period 2,
+   {:set #{7 1 15 9}
+    :ratio 1/57
+    :bounded-ratio 64/57
+    :bounding-period 2
     :cents 200.5319830023106}
-   {:set #{7 1 15 19},
-    :ratio 1/27,
-    :bounded-ratio 32/27,
-    :bounding-period 2,
+   {:set #{7 1 15 19}
+    :ratio 1/27
+    :bounded-ratio 32/27
+    :bounding-period 2
     :cents 294.1349974038373}
-   {:set #{7 15 19 9},
-    :ratio 1/3,
-    :bounded-ratio 4/3,
-    :bounding-period 2,
+   {:set #{7 15 19 9}
+    :ratio 1/3
+    :bounded-ratio 4/3
+    :bounding-period 2
     :cents 498.04499913461217}
-   {:set #{7 1 15 3},
-    :ratio 1/171,
-    :bounded-ratio 256/171,
-    :bounding-period 2,
+   {:set #{7 1 15 3}
+    :ratio 1/171
+    :bounded-ratio 256/171
+    :bounding-period 2
     :cents 698.5769821369228}
-   {:set #{7 15 3 9},
-    :ratio 1/19,
-    :bounded-ratio 32/19,
-    :bounding-period 2,
+   {:set #{7 15 3 9}
+    :ratio 1/19
+    :bounded-ratio 32/19
+    :bounding-period 2
     :cents 902.4869838676968}
-   {:set [7 15 3 19],
-    :ratio 1/9,
-    :bounded-ratio 16/9,
-    :bounding-period 2,
+   {:set [7 15 3 19]
+    :ratio 1/9
+    :bounded-ratio 16/9
+    :bounding-period 2
     :cents 996.0899982692252}])
 
 (def dorico-2v1
   "2)4 of 4)7 7.21-1.3.9.19 with missing 1st. 1st degree is sharp"
-  [{:set #{7 1 19 9},
-    :ratio 1,
-    :bounded-ratio 1,
-    :bounding-period 2,
-    :degree 0,
+  [{:set #{7 1 19 9}
+    :ratio 1
+    :bounded-ratio 1
+    :bounding-period 2
+    :degree 0
     :cents 0.0}
-   {:set #{7 1 21 9},
-    :ratio 21/19,
-    :bounded-ratio 21/19,
-    :bounding-period 2,
+   {:set #{7 1 21 9}
+    :ratio 21/19
+    :bounded-ratio 21/19
+    :bounding-period 2
     :cents 173.2678912022099}
-   {:set #{7 1 21 19},
-    :ratio 7/3,
-    :bounded-ratio 7/6,
-    :bounding-period 2,
+   {:set #{7 1 21 19}
+    :ratio 7/3
+    :bounded-ratio 7/6
+    :bounding-period 2
     :cents 266.8709056037379}
-   {:set #{7 21 19 9},
-    :ratio 21,
-    :bounded-ratio 21/16,
-    :bounding-period 2,
+   {:set #{7 21 19 9}
+    :ratio 21
+    :bounded-ratio 21/16
+    :bounding-period 2
     :cents 470.7809073345124}
-   {:set #{7 1 21 3},
-    :ratio 7/19,
-    :bounded-ratio 28/19,
-    :bounding-period 2,
+   {:set #{7 1 21 3}
+    :ratio 7/19
+    :bounded-ratio 28/19
+    :bounding-period 2
     :cents 671.3128903368225}
-   {:set #{7 21 3 9},
-    :ratio 63/19,
-    :bounded-ratio 63/38,
-    :bounding-period 2,
+   {:set #{7 21 3 9}
+    :ratio 63/19
+    :bounded-ratio 63/38
+    :bounding-period 2
     :cents 875.222892067597}
-   {:set #{7 21 3 19},
-    :ratio 7,
-    :bounded-ratio 7/4,
-    :bounding-period 2,
+   {:set #{7 21 3 19}
+    :ratio 7
+    :bounded-ratio 7/4
+    :bounding-period 2
     :cents 968.8259064691249}])
 
 (def dorico-2v2
   "2)4 of 4)7 7.21-1.3.9.19 with missing 1st.
   Sames a 2v1 but without 1st degree for practical purposes"
-  [{:set #{7 1 21 9},
-    :ratio 21/19,
-    :bounded-ratio 21/19,
-    :bounding-period 2,
+  [{:set #{7 1 21 9}
+    :ratio 21/19
+    :bounded-ratio 21/19
+    :bounding-period 2
     :cents 173.2678912022099}
-   {:set #{7 1 21 19},
-    :ratio 7/3,
-    :bounded-ratio 7/6,
-    :bounding-period 2,
+   {:set #{7 1 21 19}
+    :ratio 7/3
+    :bounded-ratio 7/6
+    :bounding-period 2
     :cents 266.8709056037379}
-   {:set #{7 21 19 9},
-    :ratio 21,
-    :bounded-ratio 21/16,
-    :bounding-period 2,
+   {:set #{7 21 19 9}
+    :ratio 21
+    :bounded-ratio 21/16
+    :bounding-period 2
     :cents 470.7809073345124}
-   {:set #{7 1 21 3},
-    :ratio 7/19,
-    :bounded-ratio 28/19,
-    :bounding-period 2,
+   {:set #{7 1 21 3}
+    :ratio 7/19
+    :bounded-ratio 28/19
+    :bounding-period 2
     :cents 671.3128903368225}
-   {:set #{7 21 3 9},
-    :ratio 63/19,
-    :bounded-ratio 63/38,
-    :bounding-period 2,
+   {:set #{7 21 3 9}
+    :ratio 63/19
+    :bounded-ratio 63/38
+    :bounding-period 2
     :cents 875.222892067597}
-   {:set #{7 21 3 19},
-    :ratio 7,
-    :bounded-ratio 7/4,
-    :bounding-period 2,
+   {:set #{7 21 3 19}
+    :ratio 7
+    :bounded-ratio 7/4
+    :bounding-period 2
     :cents 968.8259064691249}])
 
 (def dorico-3v1
@@ -215,36 +215,36 @@
     :bounded-ratio 1
     :bounding-period 2
     :cents 0.0}
-   {:set #{1 15 21 9},
-    :ratio 9,
-    :bounded-ratio 9/8,
-    :bounding-period 2,
-    :degree 6,
+   {:set #{1 15 21 9}
+    :ratio 9
+    :bounded-ratio 9/8
+    :bounding-period 2
+    :degree 6
     :cents 203.91000173077484}
-   {:set #{1 15 21 19},
-    :ratio 19,
-    :bounded-ratio 19/16,
-    :bounding-period 2,
+   {:set #{1 15 21 19}
+    :ratio 19
+    :bounded-ratio 19/16
+    :bounding-period 2
     :cents 297.5130161323026}
-   {:set #{1 15 3 19},
-    :ratio 19/7,
-    :bounded-ratio 19/14,
-    :bounding-period 2,
+   {:set #{1 15 3 19}
+    :ratio 19/7
+    :bounded-ratio 19/14
+    :bounding-period 2
     :cents 528.6871096631775}
-   {:set #{1 15 21 3},
-    :ratio 3,
-    :bounded-ratio 3/2,
-    :bounding-period 2,
+   {:set #{1 15 21 3}
+    :ratio 3
+    :bounded-ratio 3/2
+    :bounding-period 2
     :cents 701.9550008653874}
-   {:set #{7 1 15 19},
-    :ratio 19/3,
-    :bounded-ratio 19/12,
-    :bounding-period 2,
+   {:set #{7 1 15 19}
+    :ratio 19/3
+    :bounded-ratio 19/12
+    :bounding-period 2
     :cents 795.5580152669148}
-   {:set #{7 1 15 21},
-    :ratio 7,
-    :bounded-ratio 7/4,
-    :bounding-period 2,
+   {:set #{7 1 15 21}
+    :ratio 7
+    :bounded-ratio 7/4
+    :bounding-period 2
     :cents 968.8259064691249}])
 
 (def dorico-3v1
@@ -255,34 +255,34 @@
     :bounded-ratio 1
     :bounding-period 2
     :cents 0.0}
-   {:set #{7 1 21 19},
-    :ratio 133/15,
-    :bounded-ratio 133/120,
-    :bounding-period 2,
-    :degree 5,
+   {:set #{7 1 21 19}
+    :ratio 133/15
+    :bounded-ratio 133/120
+    :bounding-period 2
+    :degree 5
     :cents 178.0702078712047}
-   {:set #{1 15 21 19},
-    :ratio 19,
-    :bounded-ratio 19/16,
-    :bounding-period 2,
+   {:set #{1 15 21 19}
+    :ratio 19
+    :bounded-ratio 19/16
+    :bounding-period 2
     :cents 297.5130161323026}
-   {:set #{1 15 3 19},
-    :ratio 19/7,
-    :bounded-ratio 19/14,
-    :bounding-period 2,
+   {:set #{1 15 3 19}
+    :ratio 19/7
+    :bounded-ratio 19/14
+    :bounding-period 2
     :cents 528.6871096631775}
-   {:set #{1 15 21 3},
-    :ratio 3,
-    :bounded-ratio 3/2,
-    :bounding-period 2,
+   {:set #{1 15 21 3}
+    :ratio 3
+    :bounded-ratio 3/2
+    :bounding-period 2
     :cents 701.9550008653874}
-   {:set #{7 1 15 19},
-    :ratio 19/3,
-    :bounded-ratio 19/12,
-    :bounding-period 2,
+   {:set #{7 1 15 19}
+    :ratio 19/3
+    :bounded-ratio 19/12
+    :bounding-period 2
     :cents 795.5580152669148}
-   {:set #{7 1 15 21},
-    :ratio 7,
-    :bounded-ratio 7/4,
-    :bounding-period 2,
+   {:set #{7 1 15 21}
+    :ratio 7
+    :bounded-ratio 7/4
+    :bounding-period 2
     :cents 968.8259064691249}])
--- src/tieminos/afable_diablo/scale.clj
+++ src/tieminos/afable_diablo/scale.clj
@@ -13,6 +13,11 @@
 (defn +degree [scale]
   (map-indexed (fn [i n] (assoc n :degree i)) scale))
 
+(defn dedupe-scale [scale]
+  (->> scale
+       (group-by :bounded-ratio)
+       (map (comp first second))
+       (sort-by :bounded-ratio)))
 
 (def polydori
   (-> (cps/make 4 [1 3 9 19 15 21 7]
@@ -21,17 +26,18 @@
                 )
       cps/+all-subcps
       (update :scale (comp +cents +degree))))
+
 (comment (-> polydori :scale))
 
 (def polydori-by-sets (->> polydori :scale (group-by :set)))
 #_(-> polydori-by-sets)
 
 (comment
-  (let [{:keys [filename content]}
-        (make-scl-file (assoc polydori :scale
-                              (->> polydori :scale
-                                   (sort-by :bounded-ratio)
-                                   (group-by :bounded-ratio)
-                                   (map (comp first second))
-                                   #_(map :bounded-ratio))))]
+  (let [{:keys [filename content]} (make-scl-file (assoc polydori :scale
+                                                         (->> polydori :scale
+                                                              (group-by :bounded-ratio)
+                                                              (map (comp first second))
+                                                              (sort-by :bounded-ratio)
+                                                              #_(map :bounded-ratio))))]
     (spit (str "/Users/diego/Music/tunings/" filename) content)))
+
