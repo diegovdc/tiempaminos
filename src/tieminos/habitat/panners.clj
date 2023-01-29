b83042523de483c34e1146d7914400e1790c2ed7
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-01-29 14:29:54 -0600

    Add trayectory pan [Hábitat]

--- src/tieminos/habitat/panners.clj
+++ src/tieminos/habitat/panners.clj
@@ -48,6 +48,72 @@
                                gate
                                :action o/FREE)))))
 
+(defn trayectory-pan
+  "`trayectories` is [{:pos 0 :dur 5 :width 1.2}]. The `:dur` is the transition duration between the current node and the next,
+  and the one on the last map is not necessary.
+  After peforming the trayectory the panner will stay in the last position until it is released."
+  [{:keys [trayectories in out amp release gate]
+    :or {trayectories [{:pos 0 :dur 1 :width 2}]
+         in 0
+         out 0
+         amp 1
+         release 2}}]
+  (println trayectories)
+  (let [pos-levels (map :pos trayectories)
+        pos-durations (map :dur trayectories)
+        widths (map :width trayectories)
+        curves (repeat (count trayectories) 0.2)
+        release-node (dec (count trayectories))]
+    ((o/synth trayectory-pan-synth
+              [{:name :gate
+                :default (clojure.core/float (overtone.sc.node/to-id 1))
+                :rate :kr}]
+              (o/out out
+                     (-> (oe/circle-az :num-channels 4
+                                       :in (o/in in)
+                                       :pos (o/env-gen (o/envelope pos-levels pos-durations curves release-node))
+                                       :width (o/env-gen (o/envelope widths pos-durations curves release-node))
+                                       :orientation 0.5)
+                         (* amp (o/env-gen (o/env-adsr 1 1 1 release :curve -0.5)
+                                           gate
+                                           :action o/FREE)))))
+     (groups/mid))))
+
+(comment
+  (o/demo)
+  (groups/init-groups!)
+
+  (def test-bus (o/audio-bus 1 "test-bus"))
+  (def s ((o/synth (o/out test-bus
+                          (* 0.2 (o/lpf (o/saw 300) 1500))))
+          (groups/early)))
+
+  (o/kill s)
+  (def t (trayectory-pan {:out 0 :in test-bus}))
+
+  (def t (trayectory-pan {:trayectories [{:pos -1 :dur 1 :width 1}
+                                         {:pos 0.2 :dur 1 :width 4}
+                                         {:pos 0.71 :dur 1 :width 1}
+                                         {:pos 1.71 :dur 2 :width 2}
+                                         {:pos -0.2 :dur 1 :width 1}]
+                          :out 0 :in test-bus}))
+  (def t (trayectory-pan {:trayectories [{:pos -1 :dur 1 :width 1}
+                                         {:pos -1 :dur 1 :width 2}
+                                         {:pos -1 :dur 1 :width 3}
+                                         {:pos -1 :dur 1 :width 1}
+                                         {:pos -1 :dur 1 :width 4}
+                                         {:pos -1 :dur 1 :width 1}
+                                         {:pos -1 :dur 1 :width 4}
+                                         {:pos -1 :dur 1 :width 4}
+                                         {:pos -1 :dur 1 :width 1}
+                                         {:pos -1 :dur 1 :width 1}
+                                         {:pos -1 :dur 1 :width 1}
+                                         {:pos -1 :dur 1 :width 1}]
+                          :out 0
+                          :in test-bus}))
+  (o/ctl t :gate 0)
+  (o/stop))
+
 (defonce current-panners
   ;; "A map of input (int) to {synth, type}"
   (atom {}))
