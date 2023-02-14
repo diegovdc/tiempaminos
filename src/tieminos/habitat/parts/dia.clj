62b40ccb1b779b797720539f995b324606fb036d
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-02-14 10:46:47 -0600

    [HABITAT] Define missing parts todo

---
+++ src/tieminos/habitat/parts/dia.clj
@@ -0,0 +1,12 @@
+(ns tieminos.habitat.parts.dia
+  (:require [taoensso.timbre :as timbre]))
+
+(defn dueto-con-polinizadores
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
+
+(defn escucha-de-aves
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
---
+++ src/tieminos/habitat/parts/noche.clj
@@ -0,0 +1,27 @@
+(ns tieminos.habitat.parts.noche
+  (:require [taoensso.timbre :as timbre]))
+
+(defn de-la-montana-al-fuego
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
+
+(defn fuego
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
+
+(defn alejamiento-del-fuego
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
+
+(defn polinizadores-nocturnos
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
+
+(defn hacia-un-nuevo-universo
+  [inputs base-preouts]
+  (doseq [[k {:keys [bus]}] inputs]
+    (timbre/warn "Not implemented yet")))
