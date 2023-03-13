ea4b577a1033fb05c9dacb18d08ada56abdb32a8
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-03-13 12:52:38 -0600

    Adds reaper/time fn

--- src/tieminos/osc/reaper.clj
+++ src/tieminos/osc/reaper.clj
@@ -3,13 +3,20 @@
 
 (defonce osc-client (atom nil))
 
-(defn init [] (reset! osc-client (osc/osc-client "192.168.0.100" 1618)))
+(defn init []
+  (when-not @osc-client
+    (reset! osc-client (osc/osc-client "192.168.0.100" 1618))
+    @osc-client))
 
+(defn time [seconds]
+  "Set start time at `seconds`"
+  (osc/osc-send @osc-client "/time" (float seconds)))
 (defn play [] (osc/osc-send @osc-client "/play"))
 (defn stop [] (osc/osc-send @osc-client "/stop"))
 (defn rec [] (osc/osc-send @osc-client "/record"))
 
 (comment
+  (osc/osc-send @osc-client "/time" (float 121))
   (osc/osc-debug true)
   (osc/osc-debug false)
   (init)
