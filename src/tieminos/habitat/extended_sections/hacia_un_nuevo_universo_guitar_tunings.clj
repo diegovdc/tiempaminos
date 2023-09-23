76e6019045c14b57f4ba96b77b3fa5c283b27be1
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-09-23 14:55:03 -0600

    [HABITAT] guitar tuning for hacia un nuevo universo

---
+++ src/tieminos/habitat/extended_sections/hacia_un_nuevo_universo_guitar_tunings.clj
@@ -0,0 +1,22 @@
+(ns tieminos.habitat.extended-sections.hacia-un-nuevo-universo-guitar-tunings
+  (:require
+   [overtone.core :as o]))
+
+(def f 112)
+
+(def string->ratio {6 1
+                    5 3/2
+                    4 5/2
+                    3 7/2
+                    2 11/2
+                    1 12/2             ;maybe later 25/4 or 13/2
+                    })
+(o/defsynth tuner
+  [freq 112
+   ratio 1
+   amp 0.5]
+  (o/out 0 (* amp (o/pan2 (o/saw (* freq ratio))))))
+
+(comment
+  (tuner f (string->ratio 1))
+  (o/stop))
