0fdfea888e327b166f507760a50046d69abb5d9d
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-10-02 21:28:21 -0600

    [Habitat] add reaper markers

--- src/tieminos/osc/reaper.clj
+++ src/tieminos/osc/reaper.clj
@@ -1,4 +1,5 @@
 (ns tieminos.osc.reaper
+  (:refer-clojure :exclude [time])
   (:require [overtone.osc :as osc]))
 
 (defonce osc-client (atom nil))
@@ -8,12 +9,20 @@
     (reset! osc-client (osc/osc-client "0.0.0.0" 1618))
     @osc-client))
 
-(defn time [seconds]
-  "Set start time at `seconds`"
+(defn time "Set start time at `seconds`"
+  [seconds]
   (osc/osc-send @osc-client "/time" (float seconds)))
 (defn play [] (osc/osc-send @osc-client "/play"))
 (defn stop [] (osc/osc-send @osc-client "/stop"))
 (defn rec [] (osc/osc-send @osc-client "/record"))
+
+(defn basic-insert-marker
+  "This is a very simple way to insert markers. It may produce duplicate markers"
+  [marker-name]
+  (osc/osc-send @osc-client "insert-new-marker") ;; this osc command needs to be asociated to th action "Markers: nsert marker at current position"
+  (osc/osc-send @osc-client
+                "s/lastmarker/name"
+                marker-name))
 
 (comment
   (osc/osc-send @osc-client "/time" (float 121))
