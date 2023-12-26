a4ea558f8202c5179f289cdb65df492f6c4172e6
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-12-26 17:38:12 -0600

    Add metameantone-organ improv

---
+++ test/tieminos/lumatone/utils_test.clj
@@ -0,0 +1,32 @@
+(ns tieminos.lumatone.utils-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [tieminos.lumatone.utils :refer [multichan-mapper]]))
+
+(deftest multichan-mapper-test
+  (testing "Basic transpositions"
+    (is (= 0
+           (multichan-mapper
+             {:period/size 12}
+             {:channel 1 :note 0})))
+    (is (= 12
+           (multichan-mapper
+             {:period/size 12}
+             {:channel 2 :note 0})))
+    (is (= 36
+           (multichan-mapper
+             {:period/size 12}
+             {:channel 2 :note 24})))
+    (is (= 31
+           (multichan-mapper
+             {:period/size 31}
+             {:channel 2 :note 0})))
+    (is (= 62
+           (multichan-mapper
+             {:period/size 31}
+             {:channel 3 :note 0}))))
+  (testing "offset"
+    (is (= 24
+           (multichan-mapper
+             {:period/size 12 :offset -12}
+             {:channel 2 :note 24})))))
