d83623f93668c8620bdaed82d6fa9060127d620a
Diego Villaseñor <diego.vid.eco@gmail.com>
2024-01-19 17:56:48 -0600

    [Hábitat] Hacia un nuevo universo (amps trigger)

--- src/tieminos/habitat/extended_sections/hacia_un_nuevo_universo/scratch/amp_trig.clj
+++ src/tieminos/habitat/extended_sections/hacia_un_nuevo_universo/scratch/amp_trig.clj
@@ -21,6 +21,12 @@
     (reset! amp-trig/handlers {}))
   (init!)
 
+
+  (comment
+    ;; Amplitudes
+    {:guitar {:scarlett 4.5 :amp [5 7 8]}}
+    )
+
   (do
     ;; NOTE keep, only ps below
     (oe/defsynth synth
@@ -29,43 +35,42 @@
        ps2 1
        rz-freq 200
        out 0]
-      (o/out out (let [sig (o/mix [(o/in in 1)
-                                   ;; agregado para la toma 2
+      (o/out out (let [sig (o/mix [(o/in (-> @inputs :mic-1 :bus) 1)
                                    (o/in (-> @inputs :mic-2 :bus) 1)
-                                   ;; agregado para la toma 3, considerar controlar el volumen de la guitarra para que no exceda a la 
-                                   (o/in (-> @inputs :guitar :bus) 1)])
+                                   (* 1.5  (o/in (-> @inputs :guitar :bus) 1))])
                        sig2 (+ #_(-> sig
                                      (o/pitch-shift 0.1 ps1)
                                      (* 0.8)
                                      (#(o/pan-az 4 % (scu/lfo-kr 0.5 -1 1))))
                                (-> sig
                                    (o/pitch-shift 0.1 ps2)
-                                   (* 0.5)
+                                   #_(* 0.8)
                                    (#(o/pan-az 4 % (scu/lfo-kr 0.5 -1 1))))
                                (-> sig
                                    (o/ringz rz-freq 0.1)
                                    (* 0.07)
-;;;  TODO add limiter
+                                   (o/limiter 0.9 0.005)
                                    #_(o/lpf 8000)
-                                   (#(o/pan-az 4 % (scu/lfo-kr (scu/lfo-kr 2 1 5) -1 1)))))]
+                                   (#(o/pan-az 4 % (scu/lfo-kr (scu/lfo-kr 2 1 5) -1 1)))
+                                   ))]
                    (-> (+ sig2
                           #_(-> sig2
                                 (o/comb-l 0.2 (scu/lfo-kr 1 0.15 0.2) 1)
                                 (o/pitch-shift 0.1 ps2 0 0.0001)
                                 (* 0.5 (o/env-gen (o/env-perc 2 1  0.2 0.7)))))
-
-                       (* (o/env-gen (o/envelope [0 1 0.7 0.5 0] [0.3 3 2 5])
-                                     :time-scale 1
-                                     :action o/FREE) )
+                       (o/limiter 0.9 0.005)
+                       (*  (o/env-gen (o/envelope [0 1 1 0.65 0.6 0] [0.3 0.5 1.5 4 2])
+                                      :time-scale 1
+                                      :action o/FREE) )
                        (o/free-verb (scu/lfo-kr 2 0.4 0.7) 2 0)))))
 
     (defn amp-trig-handler
       [{:keys [in]}]
-      (println (rand))
+      (println (java.util.Date.))
       (synth {:group (groups/mid)
               :in in
               :ps1 3/2
-              :ps2 (rand-nth [1/4 11/32 #_7/2 13/32])
+              :ps2 (* (rand-nth [1 2]) (rand-nth [1/4 11/32 4/11 13/32]))
               :rz-freq (* (rand-nth [200 250 300])
                           (rand-nth [1 2 3 4 5]))
               :out percussion-processes-main-out
