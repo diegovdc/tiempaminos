8080d131633750b489335c7d781ef76e8e02211d
Diego Villaseñor <diego.vid.eco@gmail.com>
2024-01-26 10:28:23 -0600

    [Hábitat] add amp regulation function for the ps-ringz synth

--- src/tieminos/habitat/extended_sections/hacia_un_nuevo_universo/scratch/amp_trig.clj
+++ src/tieminos/habitat/extended_sections/hacia_un_nuevo_universo/scratch/amp_trig.clj
@@ -13,58 +13,59 @@
    [tieminos.habitat.scratch.sample-rec2 :refer [amanecer*guitar-clouds-2
                                                  start-rec-loop3!]]
    [tieminos.habitat.utils :refer [open-inputs-with-rand-pan]]
+   [tieminos.math.utils :refer [hyperbolic-decay linearly-weighted-avg]]
    [tieminos.overtone-extensions :as oe]
    [tieminos.sc-utils.synths.v1 :as scu]
    [tieminos.utils :refer [rrange]]
    [time-time.standard :refer [rrand]]))
 
 (defn get-rand-buf []
-    (->> @rec/bufs
-         vals
-         (sort-by :rec/time)
-         reverse
-         (filter :analysis)
-         (remove #(silence? 0.01 %))
-         (take 2)
-         (#(when (seq %) (rand-nth %)))))
+  (->> @rec/bufs
+       vals
+       (sort-by :rec/time)
+       reverse
+       (filter :analysis)
+       (remove #(silence? 0.01 %))
+       (take 2)
+       (#(when (seq %) (rand-nth %)))))
 
-  (defn play-sample [{:keys [out]}]
-    (let [buf (get-rand-buf)
-          trig-rate (weighted {40 2 100 5 80 2})
-          start (rrand 0 0.9)
-          end (rrand (+ start 0.01) 1)]
-      (when buf
-        (amanecer*guitar-clouds-2 {:group (groups/mid)
-                                   :buf buf
-                                   :a (weighted {3 3
-                                                 5 1
-                                                 0.2 1})
-                                   :d (weighted {3 3
-                                                 5 1
-                                                 0.1 1})
-                                   :r (weighted {3 3
-                                                 5 1
-                                                 2 1
-                                                 1 0.5})
-                                   :rate (weighted {1/2  2
-                                                    1    5
-                                                    3/2  5
-                                                    2    3
-                                                    11/4 2
-                                                    7/4  3})
-                                   :d-level (weighted {0.7 2 0.5 5 0.1 2})
-                                   :rev-room (rrand 2 12)
-                                   :trig-rate trig-rate
-                                   :grain-dur (/ 1 (/ trig-rate 2))
-                                   :amp-lfo (rrange 0.1 0.4)
-                                   :amp-lfo-min 0.95
-                                   :amp (weighted {1 1 0.5 5 0.7 3 0.8 2 0.9 1})
-                                   :lpf-min 300
-                                   :lpf-max (rrange 2000 10000)
-                                   :start start
-                                   :end end
-                                   :out out
-                                   :pan (rrange -1 1)}))))
+(defn play-sample [{:keys [out]}]
+  (let [buf (get-rand-buf)
+        trig-rate (weighted {40 2 100 5 80 2})
+        start (rrand 0 0.9)
+        end (rrand (+ start 0.01) 1)]
+    (when buf
+      (amanecer*guitar-clouds-2 {:group (groups/mid)
+                                 :buf buf
+                                 :a (weighted {3 3
+                                               5 1
+                                               0.2 1})
+                                 :d (weighted {3 3
+                                               5 1
+                                               0.1 1})
+                                 :r (weighted {3 3
+                                               5 1
+                                               2 1
+                                               1 0.5})
+                                 :rate (weighted {1/2  2
+                                                  1    5
+                                                  3/2  5
+                                                  2    3
+                                                  11/4 2
+                                                  7/4  3})
+                                 :d-level (weighted {0.7 2 0.5 5 0.1 2})
+                                 :rev-room (rrand 2 12)
+                                 :trig-rate trig-rate
+                                 :grain-dur (/ 1 (/ trig-rate 2))
+                                 :amp-lfo (rrange 0.1 0.4)
+                                 :amp-lfo-min 0.95
+                                 :amp (weighted {1 1 0.5 5 0.7 3 0.8 2 0.9 1})
+                                 :lpf-min 300
+                                 :lpf-max (rrange 2000 10000)
+                                 :start start
+                                 :end end
+                                 :out out
+                                 :pan (rrange -1 1)}))))
 
 (comment
   (when @habitat-initialized?
@@ -82,13 +83,44 @@
      :mic-3/lavalier {:scarlett 8 :amp-trig/threshold :default}
      :scarlett/out 8})
 
+  (def ps-ringz-amp (atom 1))
+
   (do
+    (defn set-ps-ringz-amp!
+      [peak-amp]
+      (reset! ps-ringz-amp (hyperbolic-decay peak-amp 2 0.7)))
+    (set-ps-ringz-amp! 0)
+
+    (defn init-amp-regulator-receiver!
+      []
+      (let [last-peak-amps (atom ())]
+        (o/on-event "/amp-regulator"
+                    (fn [data]
+                      (let [peak-amp (-> data :args (nth 2))
+                            last-peaks (swap! last-peak-amps (comp #(take 10 %) conj) peak-amp)
+                            peak-avg (linearly-weighted-avg last-peaks)]
+                        (set-ps-ringz-amp! peak-avg)))
+                    :amp-regulator-handler)))
+
+    (oe/defsynth amp-regulator-replier
+      [in 0
+       freq 2
+       reply-id 1]
+      (let [impulse (o/impulse freq)]
+        (o/send-reply  impulse
+                       "/amp-regulator"
+                       [(o/amplitude:kr (apply max (o/peak:ar
+                                                    (o/in:ar in 4)
+                                                    (o/delay2:kr impulse))))]
+                       reply-id)))
+
     ;; NOTE keep, only ps below
     (oe/defsynth ps-ringz
       [in 0
        ps1 1
        ps2 1
        rz-freq 200
+       amp 1
        out 0]
       (o/out out (let [sig (o/mix [(o/in (-> @inputs :mic-1 :bus) 1)
                                    (o/in (-> @inputs :mic-2 :bus) 1)
@@ -98,10 +130,10 @@
                                      (o/pitch-shift 0.1 ps1)
                                      (* 0.8)
                                      (#(o/pan-az 4 % (scu/lfo-kr 0.5 -1 1))))
-                               (-> sig
-                                   (o/pitch-shift 0.1 ps2)
-                                   #_(* 0.8)
-                                   (#(o/pan-az 4 % (scu/lfo-kr 0.5 -1 1))))
+                             (-> sig
+                                 (o/pitch-shift 0.1 ps2)
+                                 #_(* 0.8)
+                                 (#(o/pan-az 4 % (scu/lfo-kr 0.5 -1 1))))
                                (-> sig
                                    (o/ringz rz-freq 0.1)
                                    (* 0.07)
@@ -115,10 +147,10 @@
                                 (* 0.5 (o/env-gen (o/env-perc 2 1  0.2 0.7)))))
                        (o/limiter 0.9 0.005)
                        (o/free-verb (scu/lfo-kr 2 0.4 0.7) 2 0)
-                       (*  (o/env-gen (o/envelope [0 1 1 0.65 0.6 0]
-                                                  [0.3 0.5 1.5 4 2])
-                                      :time-scale 1
-                                      :action o/FREE))))))
+                       (* amp (o/env-gen (o/envelope [0 1 1 0.65 0.6 0]
+                                                     [0.3 0.5 1.5 4 2])
+                                         :time-scale 1
+                                         :action o/FREE))))))
 
     (oe/defsynth filtered-rev-long-tail
       []
@@ -133,6 +165,7 @@
                  :ps2 (* (rand-nth [1 2]) (rand-nth [1/4 11/32 4/11 13/32]))
                  :rz-freq (* (rand-nth [200 250 300])
                              (rand-nth [1 2 3 4 5]))
+                 :amp @ps-ringz-amp
                  :out percussion-processes-main-out}))
 
     (defn mic-2-amp-trig-handler
@@ -144,6 +177,7 @@
                  :ps2 (* (rand-nth [1 2]) (rand-nth [1/4 11/32 4/11 13/32]))
                  :rz-freq (* (rand-nth [200 250 300])
                              (rand-nth [1 2 3 4 5]))
+                 :amp @ps-ringz-amp
                  :out percussion-processes-main-out}))
 
     (defn mic-3-amp-trig-handler
@@ -156,12 +190,9 @@
       (println :trig/guitar (java.util.Date.))
       (play-sample {:out mixed-main-out})))
 
-
-
   (start-rec-loop3!
-    {:input-bus-fn (fn [_] (-> @inputs (select-keys [:guitar :mic-1 :mic-2 :mic-3]) vals (->> (map :bus))))
-     :durs (mapv (fn [_] (rrange 10 20)) (range 40))})
-
+   {:input-bus-fn (fn [_] (-> @inputs (select-keys [:guitar :mic-1 :mic-2 :mic-3]) vals (->> (map :bus))))
+    :durs (mapv (fn [_] (rrange 10 20)) (range 40))})
   (do
     (def mic-1-ampt (reg-amp-trigger {:in (-> @inputs :mic-1 :bus)
                                       :handler #'mic-1-amp-trig-handler}))
@@ -171,12 +202,17 @@
                                       :handler #'mic-3-amp-trig-handler}))
     (def guitar-ampt (reg-amp-trigger {:in (-> @inputs :guitar :bus)
                                        :handler #'guitar-amp-trig-handler})))
+
+  (do
+    (init-amp-regulator-receiver!)
+
+    (def ar (amp-regulator-replier
+             {:group (groups/fx)
+              :in percussion-processes-main-out})))
+
   (open-inputs-with-rand-pan
-    {:inputs inputs
-     :preouts preouts})
-
-
-  (-> mic-1-ampt)
+   {:inputs inputs
+    :preouts preouts})
 
   (do
     (amp-trig/dereg-handler mic-1-ampt)
--- src/tieminos/math/utils.clj
+++ src/tieminos/math/utils.clj
@@ -1,14 +1,14 @@
-(ns tieminos.math.utils)
+(ns tieminos.math.utils
+  (:require
+   [erv.utils.core :refer [round2]]))
 
 (defn normalize [ns]
   (let [sum (apply + ns)]
     (map #(/ % sum) ns)))
 
-
 ;; TODO implement for non lists
 ;; mapping functions
 ;; https://github.com/supercollider/supercollider/blob/18c4aad363c49f29e866f884f5ac5bd35969d828/lang/LangSource/MiscInlineMath.h
-
 
 (defn expexp [in-min in-max out-min out-max nums]
   (throw "Implement me"))
@@ -31,6 +31,9 @@
   (= (mapv float (linlin 1 5 1 3 [1 2 3 4 5]))
      [1.0, 1.5, 2.0, 2.5, 3.0]))
 
+(comment
+  (mapv float (linlin 5 0.5 1 3 [1 2 3 4 5])))
+
 (defn linexp*
   "Linear to exponential scaling"
   [in-min in-max out-min out-max x]
@@ -45,7 +48,7 @@
    (map (partial linexp* in-min in-max out-min out-max) nums)))
 
 (defn log2 [n]
-    (/ (Math/log n) (Math/log 2)))
+  (/ (Math/log n) (Math/log 2)))
 
 (defn logscale
   ;; Based on https://stackoverflow.com/a/28132981, only works when min is 0, but there are other options in this answer
@@ -69,9 +72,20 @@
     0
     (let [total (count xs)]
       (/ (apply + (map-indexed
-                    (fn [i x] (* x (- total i)))
-                    xs))
+                   (fn [i x] (* x (- total i)))
+                   xs))
          (apply + (range (inc total)))))))
+
+(defn exponentially-weighted-avg
+  [alpha xs]
+  (if-not (seq xs)
+    0
+    (let [n (count xs)]
+      (/ (apply + (map-indexed
+                   (fn [i x] (* x (Math/pow alpha (- n i))))
+                   xs))
+         (apply + (map (fn [i] (Math/pow alpha (- n i))) (range 1 (inc n))))))))
+
 
 ;; TODO add tests
 (linearly-weighted-avg [1 1 1 1])
@@ -79,3 +93,20 @@
 (linearly-weighted-avg (reverse [1 9/10 8/10 7/10 6/10 5/10]))
 (linearly-weighted-avg (range 1 -1/10 -1/10))
 (linearly-weighted-avg (reverse (range 1 -1/10 -1/10)))
+
+(defn hyperbolic-tangent
+  ([x] (hyperbolic-tangent 1 x))
+  ([scale x]
+   (/ (- (Math/pow Math/E (* scale x)) 1)
+      (+ (Math/pow Math/E (* scale x)) 1))))
+
+(defn hyperbolic-decay
+  ;; NOTE initially intended as a feedback mechanism for controlling amplitudes
+  "Works with values between 0 and 1.
+  `max-threshold` is the value after which the hyperbolic function is applied
+  `scale` is the steepness of the curve.
+
+  NOTE: This is almost surely not mathematically sound and may be better solution for which I am trying to do here."
+  [x scale max-threshold]
+  (- 1 (max 0 (hyperbolic-tangent (- x max-threshold)
+                                  scale))))
