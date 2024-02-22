2f9e47a3d520be4a505f483d9a8aa8eee8a81b37
Diego Villaseñor <diego.vid.eco@gmail.com>
2024-02-22 11:21:45 -0600

    [SC Utils] allow ndef to belong to a group

--- src/tieminos/sc_utils/ndef/v1.clj
+++ src/tieminos/sc_utils/ndef/v1.clj
@@ -6,16 +6,18 @@
 (defonce ndefs (atom {}))
 
 (defmacro ndef
-  [id synth & {:keys [out fade-time]
+  [id synth & {:keys [out fade-time group]
                :or {out 0 fade-time 3}}]
-  `(let [synth# ((o/synth
-                   [~(symbol "gate") 1]
-                  (o/out ~out
-                         (~(symbol "*") (o/env-gen
-                                (o/asr ~fade-time 1 ~fade-time)
-                                :gate ~(symbol "gate")
-                                :action o/FREE)
-                               ~synth))))]
+  `(let [synth# ~(remove nil?
+                         `((o/synth
+                               [~(symbol "gate") 1]
+                               (o/out ~out
+                                      (~(symbol "*") (o/env-gen
+                                                       (o/asr ~fade-time 1 ~fade-time)
+                                                       :gate ~(symbol "gate")
+                                                       :action o/FREE)
+                                       ~synth)))
+                           ~group))]
 
      (when-let [prev-synth# (get @ndefs ~id)]
        (try (o/ctl prev-synth# :gate 0)
@@ -30,11 +32,17 @@
       (swap! ndefs dissoc id))))
 
 (comment
+  (def my-group (o/group "my-group"))
+
   (do
     (reset! ndefs {})
     (macroexpand-1
-      '(ndef :my-id (* 0.2 (o/sin-osc 200)) {:out 0})))
+      '(ndef :my-id (* 0.2 (o/sin-osc 200)) {:out 0
+                                             :group my-group})))
 
-  (ndef :my-id (* 0.2 (o/sin-osc 100)))
+  (ndef :my-id (* 0.2 (o/sin-osc 100))
+    :group [:head my-group]
+    :fade-time 5)
   (stop :my-id)
+
   (o/stop))
