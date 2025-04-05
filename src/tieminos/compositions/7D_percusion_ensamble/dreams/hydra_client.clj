d5a19f382f25ab56cc2fb7ab759016ca28e6c0e0
Diego Villaseñor <diego.vid.eco@gmail.com>
2025-04-05 10:03:51 -0600

    [7D:dream1] hydra client ready

--- src/tieminos/compositions/7D_percusion_ensamble/dreams/hydra_client.clj
+++ src/tieminos/compositions/7D_percusion_ensamble/dreams/hydra_client.clj
@@ -4,10 +4,44 @@
 
 (defonce osc-client (atom nil))
 
-(defn init [lan-ip]
+(defn- init [lan-ip]
   (when-not @osc-client
     (reset! osc-client (osc/osc-client lan-ip 7777))
     @osc-client))
+
+(defn moment-1 []
+  (osc/osc-send @osc-client "/moment-1"))
+
+(defn play-main []
+  (osc/osc-send @osc-client "/play-main"))
+
+(defn end []
+  (osc/osc-send @osc-client "/end"))
+
+(defn to-color []
+  (osc/osc-send @osc-client "/to-color"))
+
+(defn to-gray []
+  (osc/osc-send @osc-client "/to-gray"))
+
+(defn fade-to-black []
+  (osc/osc-send @osc-client "/fade-to-black"))
+
+(defn fade-from-black []
+  (osc/osc-send @osc-client "/fade-from-black"))
+
+(defn to-init []
+  (osc/osc-send @osc-client "/to-init"))
+
+(comment
+  (moment-1)
+  (play-main)
+  (end)
+  (fade-from-black)
+  (fade-to-black)
+  (to-gray)
+  (to-color)
+  (to-init))
 
 (comment
   (init "192.168.0.104")
