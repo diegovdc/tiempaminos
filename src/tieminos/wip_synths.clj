76d60c44019a8f0ad84b78677b6807f3eb945ea0
Diego Villaseñor <diego.vid.eco@gmail.com>
2025-07-02 15:51:55 -0600

    WIP pan-az spatialization code

--- src/tieminos/wip_synths.clj
+++ src/tieminos/wip_synths.clj
@@ -11,3 +11,13 @@
                  (o/lpf 2500)
                  (o/pan2 (o/lf-noise1 pan-speed))
                  (* amp (o/env-gen (o/env-perc a (* 0.5 r)) :action o/FREE)))))
+
+(comment
+  ;; Mapping pan-az to different channels
+  (let [s (o/synth
+           (map
+            (fn [i sig]
+              (o/out i sig))
+            [0 1 2 3 0 1 1 0]
+            (o/pan-az 8 (o/sin-osc) (o/line 0 13/8 2 o/FREE))))]
+    (s)))
