10fe69b31cdf2b859e217457c7c21322e67d5dd5
Diego Villaseñor <diego.vid.eco@gmail.com>
2025-09-03 14:10:43 -0600

    [scales] create a nice version of fib[36]

---
+++ src/tieminos/scales/meru/fib.clj
@@ -0,0 +1,30 @@
+(ns tieminos.scales.meru.fib
+  (:require
+   [erv.meru.core :as meru]
+   [erv.utils.ratios :refer [ratios->scale]]
+   [tieminos.osc.surge :as surge]
+   [erv.mos.v3.core :as mos]))
+(do
+  (def fib
+    (->> {:seed [1 1 2]
+          :formula :fibonacci}
+         (meru/recurrent-series)
+         :series
+         (drop 5)
+         (take 36)
+         (ratios->scale)))
+
+  (surge/set-scale
+   {:scale {:scale fib}
+    :scale-name (:scl/name (:meta (format "dev/fib[%s]" (count fib))))}))
+
+(comment
+  (-> fib)
+  (->> (mos/gen->mos-ratios (rationalize 1.618) 2 100)
+
+       (map (juxt (comp :size :meta)
+                  (comp :mos/sL-ratio.float :meta))))
+  (surge/init)
+  (surge/set-scale
+   {:scale {:scale fib}
+    :scale-name (format "dev/fib[%s]" (count fib))}))
