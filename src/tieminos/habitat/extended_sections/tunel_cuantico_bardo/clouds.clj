c8188351cb5f03dff9877ad854c43cefeec1463b
Diego Villaseñor <diego.vid.eco@gmail.com>
2026-01-07 13:21:32 -0600

    wip add pan-fn

--- src/tieminos/habitat/extended_sections/tunel_cuantico_bardo/clouds.clj
+++ src/tieminos/habitat/extended_sections/tunel_cuantico_bardo/clouds.clj
@@ -17,12 +17,14 @@
            durs-fn
            rates-fn
            amp-fn ;; optional, takes the index and returns an amp value, if present `amp` will be overriden
+           pan-fn
            d-level-weights room-weights
            out-bus silence-thresh
            on-play]
     :or {id :clouds-refrain
          buf-fn rand-latest-buf
          amp-fn (fn [_] 1)
+         pan-fn (fn [_] {:width 1 :pos (rrange -1 1)})
          d-level-weights {0.3 1}
          room-weights {0.2 2, 2 1/2 4 1/2}
          out-bus (main-returns :non-recordable)
@@ -33,40 +35,44 @@
    :durs durs-fn
    :on-event (on-event
               (when-let [buf (buf-fn {:index index})]
-                (when-not (silence? silence-thresh buf)
-                  (let [rates (rates-fn {:index index})
-                        rates* (if (sequential? rates) rates [rates])
-                        amp* (amp-fn [{:index index}])]
-                    (doseq [r rates*]
-                      (let [trig-rate (+ 90 (rand-int 20))
-                            config {:group (groups/mid)
-                                    :buf buf
-                                    :d-level (weighted d-level-weights)
-                                    :rev-room (weighted room-weights)
-                                    :trig-rate 100
-                                    :grain-dur (/ 1 (/ trig-rate 2))
-                                    :amp-lfo (rrange 0.1 0.4)
-                                    :amp-lfo-min 0.95
-                                    :lpf-max (rrange 2000 10000)
-                                    :start 0
-                                    :end 1
-                                    :interp (rand-nth [1 2 4])
-                                    :out out-bus
-                                    :pan (rrange -1 1)}]
-                        (if on-play
-                          (on-play (assoc config
-                                          :amp amp*
-                                          :rate r
-                                          :index i))
-                          (do
-                            (amanecer*guitar-clouds (assoc config
-                                                           :rate (float r)
-                                                           :interp (rand-nth [1 2 4])
-                                                           :amp (* amp* (rrange 0.2 1) (norm-amp buf))))
-                            (amanecer*guitar-clouds (assoc config
-                                                           :rate (* (rand-nth [2 3/2 5/4 7/4 1/2 1 1 1 1]) r)
-                                                           :interp (rand-nth [4])
-                                                           :amp (* amp* (rrange 0 0.7) (norm-amp buf))))))))))))))
+                (let [rates (rates-fn {:index index})
+                      rates* (if (sequential? rates) rates [rates])
+                      amp* (amp-fn [{:index index}])]
+                  #_(println (:rec/meta buf))
+                  (doseq [r rates*]
+                    (let [trig-rate (+ 90 (rand-int 20))
+                          pan* (pan-fn {:index index
+                                        :bank (-> buf :rec/meta :subsection)})
+                          _ (println pan*)
+                          config {:group (groups/mid)
+                                  :buf buf
+                                  :d-level (weighted d-level-weights)
+                                  :rev-room (weighted room-weights)
+                                  :trig-rate 100
+                                  :grain-dur (/ 1 (/ trig-rate 2))
+                                  :amp-lfo (rrange 0.1 0.4)
+                                  :amp-lfo-min 0.95
+                                  :lpf-max (rrange 2000 10000)
+                                  :start 0
+                                  :end 1
+                                  :interp (rand-nth [1 2 4])
+                                  :out out-bus
+                                  :pan (:pos pan* 0)
+                                  :width (:width pan* 1)}]
+                      (if on-play
+                        (on-play (assoc config
+                                        :amp amp*
+                                        :rate r
+                                        :index i))
+                        (do
+                          (amanecer*guitar-clouds (assoc config
+                                                         :rate (float r)
+                                                         :interp (rand-nth [1 2 4])
+                                                         :amp (* amp* (rrange 0.2 1) (norm-amp buf))))
+                          (amanecer*guitar-clouds (assoc config
+                                                         :rate (* (rand-nth [2 3/2 5/4 7/4 1/2 1 1 1 1]) r)
+                                                         :interp (rand-nth [4])
+                                                         :amp (* amp* (rrange 0 0.7) (norm-amp buf)))))))))))))
 
 (comment
   (clouds-refrain
