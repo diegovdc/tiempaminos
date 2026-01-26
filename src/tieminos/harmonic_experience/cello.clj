4f760b9ba0a75b3600b39a06a17929b957a2cf4c
Diego Villaseñor <diego.vid.eco@gmail.com>
2026-01-25 19:26:24 -0600

    [HE] use midi->cps

--- src/tieminos/harmonic_experience/cello.clj
+++ src/tieminos/harmonic_experience/cello.clj
@@ -1,10 +1,13 @@
 (ns tieminos.harmonic-experience.cello
   (:require
+   [erv.utils.conversions :refer [midi->cps]]
    [overtone.core :as o]
    [tieminos.harmonic-experience.drones.sounds :refer [drone2]]))
 
 (comment
+
   (o/stop)
   (o/volume 0.9)
-  (drone2 65)
+
+  (drone2 (/ (midi->cps 60) 4))
   (drone2 (* 2 65)))
