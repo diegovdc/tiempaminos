802eb09d72a76621c348c0446b42b892216b3c08
Diego Villaseñor <diego.vid.eco@gmail.com>
2026-02-11 12:49:31 -0600

    Fix typo

--- src/tieminos/compositions/7D_percusion_ensamble/dreams/dream_2/nil_space.clj
+++ src/tieminos/compositions/7D_percusion_ensamble/dreams/dream_2/nil_space.clj
@@ -1,5 +1,5 @@
 (ns tieminos.compositions.7d-percusion-ensamble.dreams.dream-2.nil-space
-  "Graph for the channel distribution of the NIL (Networed Imagination Laboratory)"
+  "Graph for the channel distribution of the NIL (Networked Imagination Laboratory)"
   (:require
    [overtone.core :as o]
    [tieminos.seq-utils.utils :refer [bigraph seq->graph subgraph]]))
