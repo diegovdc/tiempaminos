dbdf1b28b7a18e78d46b6de128dc8c24500904e2
Diego Villaseñor <diego.vid.eco@gmail.com>
2026-05-01 14:55:11 -0600

    [TM] uses 18i20+BH16.128 as aggregate device

--- src/tieminos/blackhole.clj
+++ src/tieminos/blackhole.clj
@@ -28,4 +28,26 @@
                  :scarlett 20)]
     (+ offset (dec i))))
 
+(defn make-aggregate
+  "When using an aggregate device with multiple blackhole instances, return a `bus`-like function that will take as first argument the device and as second it's index."
+  [& bh-chan-sizes]
+  (let [bus-fns (->> bh-chan-sizes
+                     (reduce (fn [{:keys [offset bus-fns-map] :as acc}
+                                  ch-size]
+                               (assoc acc
+                                      :offset (+ offset ch-size)
+                                      :bus-fns-map (assoc bus-fns-map (keyword (str ch-size)) (comp bus #(+ offset %)))))
+                             {:offset 0
+                              :bus-fns-map {}})
+                     :bus-fns-map)]
+    (fn [bh-key i]
+      (if-let [bus* (bus-fns bh-key)]
+        (bus* i)
+        (throw (ex-info "Invalid blackhole instance key" {:key bh-key
+                                                          :available-keys (keys bus-fns)}))))))
 
+(comment
+  (keyword (str 16))
+  (def bus* (make-aggregate 16 128))
+  (bus* :16 1)
+  (bus* :128 1))
--- src/tieminos/tierra_mar/v1/configs.clj
+++ src/tieminos/tierra_mar/v1/configs.clj
@@ -9,8 +9,10 @@
 ;; IO
 ;;;;;;;;;;;;;;;;;;
 
+(def ^:private bh-bus (bh/make-aggregate 16 128))
+
 (def ins
-  "Buses coming in from blackhole"
+  "Buses coming in from blackhole. Will use the `Blackhole-16` devices as inputs."
   {:voz-main 2
    :fl-main 3
    :olivo/surge-pad 4                  ;; 2ch
@@ -20,25 +22,60 @@
 
 ;; First 9 bh buses are reserved as inputs
 (def outs
-  "Buses going out to blackhole"
-  {:nubosidades-fl-2ch 10
-   :nubosidades-fl2-2ch 12
-   :nubosidades-arp-2ch 14
-   :nubosidades-arp2-2ch 16
+  "Buses going out to blackhole. Will use the `Blackhole-128` device for outputs."
+  {:nubosidades-fl-2ch 1
+   :nubosidades-fl2-2ch 3
+   :nubosidades-arp-2ch 5
+   :nubosidades-arp2-2ch 7
 
-   :olivo-spiral-arp-28ch 18 ;; until 45
-   :olivo-tree-top-30ch 46   ;; until 75
+   :olivo-spiral-arp-28ch 9 ;; until 36
+   :olivo-tree-top-30ch 37   ;; until 75
 
-   :lluvia-voice-dome-25ch 76 ;; until 100 (actually 101, but last channel in REAPER is not used, but must be a pair number for REAPER)
-   :lluvia-voice-shadow-2ch 101 ;; until 102
+   :lluvia-voice-dome-25ch 67 ;; until 92 (actually 91, but last channel in REAPER is not used, but must be a pair number for REAPER)
+   :lluvia-voice-shadow-2ch 92 ;; until 93
 
-   :lluvia-fl-spirals-22ch 103 ;; until 124
-   :lluvia-fl-shadow-2ch 125   ;; until 126
+   :lluvia-fl-spirals-22ch 94 ;; until 124
+   :lluvia-fl-shadow-2ch 116   ;; until 126
 
    ;; NOTE: The next section overlaps with the `nubosidades`and `olivo`, but they shouldn't be sounding by now. Tried using Blackhole 256, but Supercollider wouldn't start, at least not with the Aggregate device.
-   :campo-magentismo-51ch 10 ;; until 61
+   :campo-magentismo-51ch 1 ;; until 61
    })
+
+#_'([:nubosidades-fl-2ch 1]
+    [:nubosidades-fl2-2ch 3]
+    [:nubosidades-arp-2ch 5]
+    [:nubosidades-arp2-2ch 7]
+
+    [:olivo-spiral-arp-28ch 9]
+    [:olivo-tree-top-30ch 37]
+
+    [:lluvia-voice-dome-25ch 67]
+    [:lluvia-voice-shadow-2ch 92]
+
+    [:lluvia-fl-spirals-22ch 94]
+    [:lluvia-fl-shadow-2ch 116]
+
+    [:campo-magentismo-51ch 1])
 (comment
+  (->> {:nubosidades-fl-2ch 10
+        :nubosidades-fl2-2ch 12
+        :nubosidades-arp-2ch 14
+        :nubosidades-arp2-2ch 16
+
+        :olivo-spiral-arp-28ch 18 ;; until 45
+        :olivo-tree-top-30ch 46   ;; until 75
+
+        :lluvia-voice-dome-25ch 76 ;; until 100 (actually 101, but last channel in REAPER is not used, but must be a pair number for REAPER)
+        :lluvia-voice-shadow-2ch 101 ;; until 102
+
+        :lluvia-fl-spirals-22ch 103 ;; until 124
+        :lluvia-fl-shadow-2ch 125   ;; until 126
+
+        ;; NOTE: The next section overlaps with the `nubosidades`and `olivo`, but they shouldn't be sounding by now. Tried using Blackhole 256, but Supercollider wouldn't start, at least not with the Aggregate device.
+        :campo-magentismo-51ch 10 ;; until 61
+        }
+
+       (map (juxt first (comp #(- % 9) second))))
   ;; total channels
   (+ 9
      2 2 2 2
@@ -48,12 +85,12 @@
 
 (defn get-input [k]
   (if-let [bus (ins k)]
-    (bh/bus bus)
+    (bh-bus :16 bus)
     (throw (ex-info "In bus not found" {:key k}))))
 
 (defn get-output [k]
   (if-let [bus (outs k)]
-    (bh/bus bus)
+    (bh-bus :128 bus)
     (throw (ex-info "Out bus not found" {:key k}))))
 
 ;; IO Buses
