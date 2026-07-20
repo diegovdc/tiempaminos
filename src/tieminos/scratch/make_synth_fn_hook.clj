679265d5ce75d4304c62a4e570947e83eac8a1c1
Diego Villaseñor <diego.vid.eco@gmail.com>
2026-07-20 11:58:20 -0600

    [hooks] add test file for make-synth-fn

---
+++ src/tieminos/scratch/make_synth_fn_hook.clj
@@ -0,0 +1,32 @@
+(ns tieminos.scratch.make-synth-fn-hook
+  (:require
+   [overtone.core :as o]
+   [tieminos.sc-utils.synths.template-synth.v0 :refer [make-synth-fn]]))
+
+(comment
+
+  (make-synth-fn
+   'siny
+   (-> {:freq [500 900]
+        :amp 1})
+   '(let [sig (o/sin-osc freq)]
+      (-> sig
+          (:ugen/freq-mixer freq amp)
+          (:ugen/outs))))
+
+  (make-synth-fn
+   'siny
+   (-> {:freq [500 900]
+        :amp 1})
+   '(let [sig (o/sin-osc freq)]
+      (-> sig
+          (:ugen/outs)
+          (:ugen/freq-mixer freq amp))))
+
+  (make-synth-fn
+   'siny
+   (-> {:freq [500 900]
+        :amp 1})
+   '(let [sig (o/sin-osc freq)]
+      (-> sig
+          (:ugen/freq-mixer freq amp)))))
