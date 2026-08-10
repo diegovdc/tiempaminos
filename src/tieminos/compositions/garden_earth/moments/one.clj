981c40c16ec14efbd0e34bb9aae9e6f5b706e3fc
Diego Villaseñor <diego.vid.eco@gmail.com>
2026-08-09 22:48:53 -0600

    [GE] refactor one.clj to use re-affect

--- src/tieminos/compositions/garden_earth/moments/one.clj
+++ src/tieminos/compositions/garden_earth/moments/one.clj
@@ -5,6 +5,7 @@
    [clojure.string :as str]
    [erv.scale.core :refer [+names]]
    [overtone.core :as o]
+   [re-affect.alpha.core :as ræ]
    [taoensso.timbre :as timbre]
    [tieminos.compositions.7d-percusion-ensamble.base :refer [bh]]
    [tieminos.compositions.garden-earth.analysis
@@ -25,6 +26,11 @@
    [tieminos.utils :refer [wrap-at]]
    [time-time.dynacan.players.gen-poly :as gp :refer [on-event ref-rain]]))
 
+(defonce ^:private live-state (atom {}))
+(ræ/reg-state ::db live-state)
+(ræ/defapi ::db)
+(comment
+  (-> @ræ/states))
 (def arp-subcps
   [;; S.0
    ["2)4 of 3)6 11-1.5.7.9"
@@ -107,6 +113,19 @@
            :harmonizer/harmony harmony
            :harmonizer/harmony-str (str (into [] harmony) " - " subcps-name* " on " pitch-class " " set*))))
 
+(defn get-harmonizer-data
+  [harmony-index section]
+  (let [index harmony-index
+        [root-deg subcps-name] (->> section :harmonizer :harmonies
+                                    (wrap-at index))
+        {:keys [root harmony]} (make-harmony root-deg subcps-name)
+        subcps-name* (str/replace subcps-name #"of 3\)6" "")
+        pitch-class (-> root :pitch :class)
+        set* (->> root :set sort (str/join ".") (#(str "{" % "}")))]
+    {:harmonizer/harmony-index index
+     :harmonizer/harmony harmony
+     :harmonizer/harmony-str (str (into [] harmony) " - " subcps-name* " on " pitch-class " " set*)}))
+
 (defn update-arp-pattern
   [{:keys [arp/pattern-index section] :as state}]
   (let [index (inc (or pattern-index 0))
@@ -116,6 +135,15 @@
     (assoc state
            :arp/pattern-index index
            :arp/pattern pattern)))
+
+(defn get-arp-pattern-data
+  [pattern-index section]
+  (let [index pattern-index
+        pattern (->> section :arp :patterns
+                     (wrap-at index))]
+    {:arp/pattern-index index
+     :arp/pattern-name (:name pattern)
+     :arp/pattern-fn (:fn pattern)}))
 
 (defn update-arp-scale-data
   [{:keys [arp/cps-index section] :as state}]
@@ -131,14 +159,57 @@
                               (str/join " " (map (comp :class :pitch) scale))]
            :arp/scale scale)))
 
-(def ^:private initial-state (-> {:section 0}
-                                 update-arp-scale-data
-                                 update-arp-pattern))
+(defn get-arp-scale-data
+  [cps-index section]
+  (let [index cps-index
+        cps (-> section :arp :cps)
+        subcps-name (wrap-at index cps)
+        scale (subcps subcps-name)]
+    {:arp/cps-index  index
+     :arp/subcps-name subcps-name
+     :arp/harmony-strs [(str/replace subcps-name #"of 3\)6" "")
+                        (str/join " " (map (comp :class :pitch) scale))]
+     :arp/scale scale}))
 
-(defonce ^:private live-state (atom initial-state))
+(def sections*
+  [{:arp {:cps ["2)4 of 3)6 11-1.5.7.9"
+                "2)4 of 3)6 9-1.5.7.11"]
+          :patterns (nth arp-patterns 0)}
+    :harmonizer {:harmonies (nth harmonizer-harmonies 0)}}])
+
+(def post-live-state-fx {::post-live-state (fn [_ _] {::post-live-state nil})})
+
+(reg-sub ::arp-cps-data
+         (fn [{:keys [arp/cps-index section]}]
+           (get-arp-scale-data cps-index (wrap-at section sections*)))
+         post-live-state-fx)
+
+(reg-sub ::arp-pattern-data
+         (fn [{:keys [arp/pattern-index section]}]
+           (get-arp-pattern-data pattern-index (wrap-at section sections*)))
+         post-live-state-fx)
+
+(reg-sub ::harmonizer-data
+         (fn [{:keys [harmonizer/harmony-index section]}]
+           (get-harmonizer-data harmony-index (wrap-at section sections*)))
+         (merge post-live-state-fx
+                {::restart-harmonizer (fn [{:keys [db]} _]
+                                        (when (:harmonizer/on? db)
+                                          {::start-harmonizer {}}))}))
+
+(comment
+  (dispatch {::inc-arp-cps-index {}})
+  (dispatch {::inc-arp-pattern-index {}})
+  (dispatch {::inc-harmonizer-harmony-index {}})
+  (get-subval ::arp-cps-data)
+  (get-subval ::arp-pattern-data))
+
+(comment
+  (add-watch live-state
+             ::live-state-portal
+             (fn [_ _ _ s] (user/tap :live-state s))))
 
 (defonce ^:private last-sets (atom '()))
-
 (defn on-receive-pitch
   [{:keys [pitch-class diff-cents]
     :as freq-analysis-data}]
@@ -152,9 +223,9 @@
                              :label (format "%s {%s}" pitch-class (str/join "." set*))
                              :last-sets @last-sets
                              :diff-cents diff-cents))))
+
 ;; Signal analyzer
 ;; Expects the webapp to be running
-
 (defn start-signal-analyzer!
   [in]
   (start-signal-analyzer {:in in
@@ -175,10 +246,28 @@
 
 (defn stop-sample-arp! []
   (timbre/info :stopping-arp)
-  (gp/stop ::arp-rain)
-  (swap! live-state assoc
-         :arp.refrain/on? false))
-(-> @live-state)
+  (gp/stop ::arp-rain))
+
+(defn start-sample-arp!
+  []
+  (timbre/info :starting-arp)
+  (ref-rain :id ::arp-rain
+            :durs [5 3 8 2 1 5]
+            :ratio 1/3
+            :on-event (on-event
+                       (let [scale (:arp/scale (get-subval ::arp-cps-data))
+                             pattern-fn (:arp/pattern-fn (get-subval ::arp-pattern-data))]
+                         (if-not (or scale pattern-fn)
+                           (timbre/warn ":arp-rain needs scale and pattern" {:scale scale :pattern pattern-fn})
+                           (arp {:bufs-atom sc.rec.v1/bufs
+                                 :dur 0.5
+                                 :index index
+                                 :in (ge.route/fl-i1 :bus)
+                                 :play-fn #_(partial #'arp-reponse-1 {:scale scale
+                                                                      :out (bh 0)})
+                                 (partial #'arp-reponse-2 {:scale scale
+                                                           :interval-seq-fn (or pattern-fn default-interval-seq-fn)
+                                                           :out (bh 2)})}))))))
 (defn make-repeat-cell
   [pattern-cell
    pitch-class
@@ -195,26 +284,6 @@
 
     (map #(interval-from-pitch-class2 scale pitch-class %)
          pattern)))
-
-(defn start-sample-arp!
-  [{:keys [subcps-name interval-seq-fn]
-    :or {interval-seq-fn default-interval-seq-fn}}]
-  (timbre/info :starting-arp)
-  (ref-rain :id ::arp-rain
-            :durs [5 3 8 2 1 5]
-            :ratio 1/3
-            :on-event (on-event
-                       (let [{:keys [arp/scale arp/pattern]} @live-state]
-                         (arp {:bufs-atom sc.rec.v1/bufs
-                               :dur 0.5
-                               :index index
-                               :in (ge.route/fl-i1 :bus)
-                               :play-fn #_(partial #'arp-reponse-1 {:scale scale
-                                                                    :out (bh 0)})
-                               (partial #'arp-reponse-2 {:scale scale
-                                                         :interval-seq-fn (:fn pattern)
-                                                         :out (bh 2)})}))))
-  (swap! live-state assoc :arp.refrain/on? true))
 
 ;;;;;;;;;;;;;;;
 ;;; Harmonizer
@@ -234,23 +303,25 @@
      :harmony (->> scale
                    (map (fn [d] (/ (:bounded-ratio d) root-ratio)))
                    (remove #(= 1 %)))}))
-(comment
-  (make-harmony 0 "3)4 of 3)6 1.3.5.9"))
 
+(comment
+  (make-harmony 0 "3)4 of 3)6 1.3.5.9")
+  (:harmonizer/harmony (get-subval ::harmonizer-data)))
 (defn start-harmonizer! []
-  (timbre/info :starting-harmonizer)
-  (if-let [ratios (:harmonizer/harmony @live-state)]
-    (do (ndef/ndef
-         ::harmonizer
-         (-> (o/sound-in (ge.route/fl-i1 :in))
-             #_(o/delay-l 1 1)
-             (o/pitch-shift 0.1 ratios)
-             ((fn [sig] (if (> (count ratios) 1) (o/mix sig) sig)))
-             (o/free-verb 0.5 3)
-             (o/pan2)
-             (* 8))
-         {:out (bh 2)})
-        (swap! live-state assoc :harmonizer/on? true))
+  (timbre/info "(re)starting-harmonizer")
+  (if-let [ratios (:harmonizer/harmony (get-subval ::harmonizer-data))]
+    (do
+      (println ratios)
+      (ndef/ndef
+       ::harmonizer
+       (-> (o/sound-in (ge.route/fl-i1 :in))
+           #_(o/delay-l 1 1)
+           (o/pitch-shift 0.1 ratios)
+           ((fn [sig] (if (> (count ratios) 1) (o/mix sig) sig)))
+           (o/free-verb 0.5 3)
+           (o/pan2)
+           (* 8))
+       {:out (bh 2)}))
     (timbre/error "No :harmonizer/harmony found")))
 
 (comment
@@ -276,24 +347,106 @@
                                                ["2)4 of 3)6 11-1.5.7.9"
                                                 "2)4 of 3)6 9-1.5.7.11"])
                          :interval-seq-fn (partial make-repeat-cell
-                                                   (wrap-at (:arp/pattern-fn-index live-state-data 0)
+                                                   (wrap-at (:arp/pattern-index live-state-data 0)
                                                             [[0 2]
                                                              [0 -2]
                                                              [0 3 1 -2]]))})}}]
 
     ((get-in sections* [(:section live-state-data 0) config-key]))))
 
+(def ^:private initial-state (-> {:section 0}
+                                 update-arp-scale-data
+                                 update-arp-pattern))
+
+(defn init! []
+  (o/stop) (reset! live-state initial-state)
+  (ge.init/init!))
+
+(declare reg-event-db dispatch)
+
+(reg-event-db
+ ::change-section
+ (fn [db {:keys [inc?]}]
+   (let [op (if inc? inc dec)]
+     (update db :section op))))
+
+(reg-event-db
+ ::inc-arp-cps-index
+ (fn [db _]
+   (update db :arp/cps-index (fnil inc 0))))
+
+(reg-event-db
+ ::inc-arp-pattern-index
+ (fn [db _]
+   (update db :arp/pattern-index (fnil inc 0))))
+
+(reg-event-db
+ ::inc-harmonizer-harmony-index
+ (fn [db _]
+   (update db :harmonizer/harmony-index (fnil inc 0))))
+
+(reg-event-fx
+ ::toggle-sample-arp
+ (fn [{:keys [db]} _]
+   (let [on? (:arp.refrain/on? db)]
+     {:db (assoc db :arp.refrain/on? (not on?))
+      :fx (if on?
+            {::stop-sample-arp {}}
+            {::start-sample-arp {}})})))
+
+(reg-event-fx
+ ::toggle-harmonizer
+ (fn [{:keys [db]} _]
+   (let [on? (:harmonizer/on? db)]
+     {:db (assoc db :harmonizer/on? (not on?))
+      :fx (if on?
+            {::stop-harmonizer {}}
+            {::start-harmonizer {}})})))
+
+(reg-fx ::stop-sample-arp (fn [_ _] (stop-sample-arp!)))
+
+(reg-fx ::start-sample-arp (fn [_ _] (start-sample-arp!)))
+
+(reg-fx ::stop-harmonizer (fn [_ _] (stop-harmonizer!)))
+
+(reg-fx ::start-harmonizer (fn [_ _] (start-harmonizer!)))
+
+(defn post-live-state*
+  [live-state-data]
+  (post-live-state (-> live-state-data
+                       (update :arp/pattern :name)
+                       (merge (get-subval ::arp-cps-data)
+                              {:arp/pattern (:arp/pattern-name (get-subval ::arp-pattern-data))}
+                              (get-subval ::harmonizer-data)))))
+
+(reg-fx ::post-live-state
+        (fn [{:keys [db]} _] (post-live-state* db)))
+
 (comment
-  (do (o/stop) (reset! live-state initial-state))
-  (ge.init/init!)
-  #_(-> @live-state)
+  (ræ/get-state ::db)
+  (dispatch {::change-section {:inc? false}})
+  (dispatch {::change-section {:inc? true}})
+
+  (get-subval ::arp-cps-data)
+  (dispatch {::toggle-sample-arp {}})
+  (dispatch {::inc-arp-cps-index {}})
+  (dispatch {::inc-arp-pattern-index {}})
+
+  (get-subval ::harmonizer-data)
+  (dispatch {::inc-harmonizer-harmony-index {}})
+  (dispatch {::toggle-harmonizer {}})
+
+  (post-live-state* @live-state))
+
+(comment
+  (init!)
+  (-> @live-state)
   (start-signal-analyzer! (ge.route/fl-i1 :in))
 
   ;; init live-state
   (add-watch live-state ::post-live-state
              (fn [_key _ref _old-value new-value]
-               (post-live-state (-> new-value
-                                    (update :arp/pattern :name)))))
+               (post-live-state* new-value)))
   (->> @live-state)
   (pan-verb :in (ge.route/fl-i1 :in) :amp 2 :mix 1 :room 1
             :damp-min 0.6 :damp 0.7
@@ -305,27 +458,16 @@
    :note-on (fn [{:keys [note]}]
               (println note)
               (cond
-                 ;; set section
-                (= 2 note) (swap! live-state update :section dec)
-                (= 3 note) (swap! live-state update :section inc)
-                 ;; arp
-                (and (:arp.refrain/on? @live-state)
-                     (= 4 note))
-                (stop-sample-arp!)
+                ;; set section
+                (= 2 note) (dispatch {::change-section {:inc? false}})
+                (= 3 note) (dispatch {::change-section {:inc? true}})
+                ;; arp
+                (= 4 note) (dispatch {::toggle-sample-arp {}})
 
-                (= 4 note) (start-sample-arp! (sections :arp @live-state))
+                ;; arp config
+                (= 5 note) (dispatch {::inc-arp-cps-index {}})
+                (= 6 note) (dispatch {::inc-arp-pattern-index {}})
 
-                 ;; arp config
-                (= 5 note) (swap! live-state update-arp-scale-data)
-                (= 6 note) (swap! live-state update-arp-pattern)
-
-                 ;; harmonizer
-                (and (:harmonizer/on? @live-state)
-                     (= 7 note))
-                (stop-harmonizer!)
-
-                (= 7 note) (start-harmonizer!)
-                (= 8 note) (do
-                             (swap! live-state update-harmonizer-harmony)
-                             (when (:harmonizer/on? @live-state)
-                               (start-harmonizer!)))))))
+                ;; harmonizer
+                (= 7 note) (dispatch {::toggle-harmonizer {}})
+                (= 8 note) (dispatch {::inc-harmonizer-harmony-index {}})))))
